6a1ba58310d217b1a5f1de5c27845fe886a630fb
Rob Tabuteau <robtabuteau@Robs-MacBook-Air.local>
2025-06-05 20:24:43 +1000

    Wrap app in TradesProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
--import JournalPage from "./pages/JournalPage";
--import PerformancePage from "./pages/PerformancePage";
-+import JournalPage from "./pages/JournalPage";
-+import PerformancePage from "./pages/PerformancePage";
-+import { TradesProvider } from "./context/TradesContext";
-
+import JournalPage from "./pages/JournalPage";
+import PerformancePage from "./pages/PerformancePage";
+import { TradesProvider } from "./context/TradesContext";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-+    {/* TradesProvider makes { trades, setTrades } available everywhere */}
-+    <TradesProvider>
+    <TradesProvider>
       <Router>
         <Routes>
           <Route path="/" element={<JournalPage />} />
           <Route path="/performance" element={<PerformancePage />} />
         </Routes>
       </Router>
-+    </TradesProvider>
+    </TradesProvider>
   </React.StrictMode>
 );
