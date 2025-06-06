72d6440038f40eb09665ef7810df710f66426eea
Rob Tabuteau <robtabuteau@Robs-MacBook-Air.local>
2025-06-06 14:29:10 +1000

    Wrap router in TradesProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { TradesProvider } from "./context/TradesContext";
 import JournalPage from "./pages/JournalPage";
 import PerformancePage from "./pages/PerformancePage";
-import { TradesProvider } from "./context/TradesContext";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
